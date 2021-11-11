55665c9c7eb3bc4ceba416313fdcb731075c7010
Candice Ariel <candicewangsa@gmail.com>
2021-11-11 20:01:53 +0700

    footer adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Footer() {
     return (
         <div className='footer-container d-flex align-items-center justify-content-center'>
-            <div className=''>
+            <div className='logo-footer'>
                 <img src='/images/publicdomain.png' alt="logo" />
             </div>
             <div className=''>
-                <p>To the extent possible under law, <a rel="dct:publisher" href="https://cryptoadz.io/"><span>Gremplin</span></a> has waived all copyright and related or neighboring rights to<span property="dct:title" class="pl-1">CrypToadz by Gremplin</span>. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" about="https://cryptoadz.io/" class="px-1">United States</span>.</p>
+                To the extent possible under law, <a rel="dct:publisher" href="https://cryptoadz.io/"><span>Gremplin</span></a> has waived all copyright and related or neighboring rights to<span property="dct:title" class="pl-1">CrypToadz by Gremplin</span>. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" about="https://cryptoadz.io/" class="px-1">United States</span>.
             </div>
         </div>
     )
