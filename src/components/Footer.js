5837d23748ec6811f8af6c99d72568b866d52e3a
Candice Ariel <candicewangsa@gmail.com>
2021-11-11 16:00:43 +0700

    image and text containe

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <div className='col-md-1'>
+                <img src='/images/publicdomain.png' alt="logo" />
+            </div>
+            <div className='col-md-10'>
+                <h4>To the extent possible under law, <a rel="dct:publisher" href="https://cryptoadz.io/"><span>Gremplin</span></a> has waived all copyright and related or neighboring rights to<span property="dct:title" class="pl-1">CrypToadz by Gremplin</span>. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" about="https://cryptoadz.io/" class="px-1">United States</span>.</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/Home.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className='container-fluid '>
+                <img src='/images/cryptoadz-logo.png' className='img-logo' alt="logo" />
+                <div className='price'>
+                    <i className="fab fa-ethereum fa-3x">.069 each</i>
+                    <h3>/6969</h3>
+                </div>
+            </div>
+            <div className='faq'>
+                <div className='text-container'>
+                    <h1 className="text-uppercase">Cryptoadz</h1>
+                    <p>CrypToadz are small amphibious creatures that roam the swampy basin of what was formerly known as Uniswamp. Unfortunately, approximately six weeks ago the Evil King Gremplin descended upon Uniswamp and dispatched the CrypToadz leader Colonel Floorbin, exiling him to Gooch Island. Following the fall of Colonel Floorbin, the Evil King Gremplin declared his divine right to rule over all CrypToadz in the Metaverse. The Evil King Gremplin's first order was to rename Uniswamp to GREMPLAND. The Evil King Gremplin rules over the CrypToadz along with his army of Fronkz. The Toadz have been known to refer to the Evil King Gremplin's reign as "THE CROAKENING". Help rid the Toadz of the rule of Evil King Gremplin by choosing how many to free during minting.</p>
+                </div>
+                <div className='img-container gridImage'>
+                    <div className='imgBox'>
+                        <img src='/images/401.png' className='faq-img' alt='401'/>
+                    </div>
+                    <div className='imgBox'>
+                        <img src='/images/76.png' className='faq-img' alt='76'/>
+                    </div>
+                    <div className='imgBox'>
+                        <img src='/images/15232.png' className='faq-img' alt='15232'/>
+                    </div>
+                    <div className='imgBox'>
+                        <img src='/images/12221.png' className='faq-img' alt='15232'/>
+                    </div>
+                </div>
+            </div>
+            <div className='about'>
+                <div className='img-container'>
+                    <div className=''>
+                        <img src='/images/temp_gremp.png' className='about-img' alt='gremplin'/>
+                    </div>
+                </div>
+                <div className='text-container'>
+                    <h1 className="text-uppercase">Gremplin</h1>
+                    <p>On a January evening in early 2021, Gremplin scurried through dark vents and damp pipes after making two wrong turns back home to the depths of his hole. When he finally surfaced, trapped in the chaotic energy of the metaverse, he scrambled to keep pace. He crafted spinning boxes to fit in with the crowd, and drew rotating residents of his homeland to help people understand where he’s from. Eventually, he took to drawing residents of this new land to pass the time. Finally feeling at home, he began to seek out ways to help build in this place. He’s just doing what he’s always done - drawing residents of lands, be they Nouns or Toadz - just trying to find his way back to his hole.</p>
+                </div>
+            </div>
+            <div className='member'>
+                <div className='container-fluid'>
+                    <h1 className='text-center'>
+                        Made with love
+                    </h1>
+                </div>
+                <div className='member-img'>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/bn.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>BN</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/west.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>West</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/chanzero.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>Chanzero</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/dinfo.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>Dinfo</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/emmy.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>Emmy</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/heeeee.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>heeeee</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/motivate.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>Motivateme</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/sum1.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>sum1</h4>
+                    </div>
+                    <div className='col-md-1 member-img-box'>
+                        <img src='/images/temp_gremp.png' className='faq-img' alt='401'/>
+                        <h4 className='text-center'>gremplin</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home
