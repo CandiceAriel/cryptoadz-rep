9191a7df791675d9536a7f875b3c5618ae1463da
Candice Ariel <candicewangsa@gmail.com>
2021-11-11 19:09:17 +0700

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='footer-container'>
-            <div className='col-md-1'>
+        <div className='footer-container d-flex align-items-center justify-content-center'>
+            <div className=''>
                 <img src='/images/publicdomain.png' alt="logo" />
             </div>
-            <div className='col-md-10'>
-                <h4>To the extent possible under law, <a rel="dct:publisher" href="https://cryptoadz.io/"><span>Gremplin</span></a> has waived all copyright and related or neighboring rights to<span property="dct:title" class="pl-1">CrypToadz by Gremplin</span>. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" about="https://cryptoadz.io/" class="px-1">United States</span>.</h4>
+            <div className=''>
+                <p>To the extent possible under law, <a rel="dct:publisher" href="https://cryptoadz.io/"><span>Gremplin</span></a> has waived all copyright and related or neighboring rights to<span property="dct:title" class="pl-1">CrypToadz by Gremplin</span>. This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" about="https://cryptoadz.io/" class="px-1">United States</span>.</p>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,79 +10,79 @@ function Home() {
                     <h1>/6969</h1>
                 </div>
             </div>
-            <div className='faq'>
-                <div className='text-container'>
+            <div className='faq d-flex align-items-center justify-content-center'>
+                <div className='text-container col-md-6'>
                     <h1 className="text-uppercase">Cryptoadz</h1>
                     <p>CrypToadz are small amphibious creatures that roam the swampy basin of what was formerly known as Uniswamp. Unfortunately, approximately six weeks ago the Evil King Gremplin descended upon Uniswamp and dispatched the CrypToadz leader Colonel Floorbin, exiling him to Gooch Island. Following the fall of Colonel Floorbin, the Evil King Gremplin declared his divine right to rule over all CrypToadz in the Metaverse. The Evil King Gremplin's first order was to rename Uniswamp to GREMPLAND. The Evil King Gremplin rules over the CrypToadz along with his army of Fronkz. The Toadz have been known to refer to the Evil King Gremplin's reign as "THE CROAKENING". Help rid the Toadz of the rule of Evil King Gremplin by choosing how many to free during minting.</p>
                 </div>
-                <div className='img-container gridImage'>
+                <div className='img-container  col-md-4 gridImage'>
                     <div className='imgBox'>
-                        <img src='/images/401.png' className='faq-img' alt='401'/>
+                        <img src='/images/401.png' className='about-img' alt='401'/>
                     </div>
                     <div className='imgBox'>
-                        <img src='/images/76.png' className='faq-img' alt='76'/>
+                        <img src='/images/76.png' className='about-img' alt='76'/>
                     </div>
                     <div className='imgBox'>
-                        <img src='/images/15232.png' className='faq-img' alt='15232'/>
+                        <img src='/images/15232.png' className='about-img' alt='15232'/>
                     </div>
                     <div className='imgBox'>
-                        <img src='/images/12221.png' className='faq-img' alt='15232'/>
+                        <img src='/images/12221.png' className='about-img' alt='15232'/>
                     </div>
                 </div>
             </div>
-            <div className='about'>
-                <div className='img-container'>
+            <div className='gremplin d-flex align-items-center justify-content-center'>
+                <div className='img-container col-md-4'>
                     <div className=''>
-                        <img src='/images/temp_gremp.png' className='about-img' alt='gremplin'/>
+                        <img src='/images/temp_gremp.png' className='gremplin-img' alt='gremplin'/>
                     </div>
                 </div>
-                <div className='text-container'>
+                <div className='text-container col-md-6'>
                     <h1 className="text-uppercase">Gremplin</h1>
                     <p>On a January evening in early 2021, Gremplin scurried through dark vents and damp pipes after making two wrong turns back home to the depths of his hole. When he finally surfaced, trapped in the chaotic energy of the metaverse, he scrambled to keep pace. He crafted spinning boxes to fit in with the crowd, and drew rotating residents of his homeland to help people understand where he’s from. Eventually, he took to drawing residents of this new land to pass the time. Finally feeling at home, he began to seek out ways to help build in this place. He’s just doing what he’s always done - drawing residents of lands, be they Nouns or Toadz - just trying to find his way back to his hole.</p>
                 </div>
             </div>
-            <div className='member'>
+            <div className='members'>
                 <div className='container-fluid'>
                     <h1 className='text-center'>
-                        Made with love
+                        Made with love,
                     </h1>
                 </div>
-                <div className='member-img'>
+                <div className='member'>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/bn.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>BN</h4>
+                        <img src='/images/bn.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>BN</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/west.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>West</h4>
+                        <img src='/images/west.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>West</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/chanzero.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>Chanzero</h4>
+                        <img src='/images/chanzero.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>Chanzero</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/dinfo.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>Dinfo</h4>
+                        <img src='/images/dinfo.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>Dinfo</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/emmy.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>Emmy</h4>
+                        <img src='/images/emmy.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>Emmy</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/heeeee.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>heeeee</h4>
+                        <img src='/images/heeeee.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>heeeee</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/motivate.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>Motivateme</h4>
+                        <img src='/images/motivate.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>Motivateme</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/sum1.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>sum1</h4>
+                        <img src='/images/sum1.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>sum1</h5>
                     </div>
                     <div className='col-md-1 member-img-box'>
-                        <img src='/images/temp_gremp.png' className='faq-img' alt='401'/>
-                        <h4 className='text-center'>gremplin</h4>
+                        <img src='/images/temp_gremp.png' className='member-img' alt='401'/>
+                        <h5 className='text-center'>gremplin</h5>
                     </div>
                 </div>
             </div>
