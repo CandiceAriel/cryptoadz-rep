66a663bca05b459bf24e1f41aa4ee25fb2cc80c1
Candice Ariel <candicewangsa@gmail.com>
2021-11-11 21:12:39 +0700

    margin-padding adjustment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,11 @@ function Home() {
                 </div>
             </div>
             <div className='faq d-flex align-items-center justify-content-center'>
-                <div className='text-container col-md-6'>
+                <div className='text-container'>
                     <h1 className="text-uppercase">Cryptoadz</h1>
                     <p>CrypToadz are small amphibious creatures that roam the swampy basin of what was formerly known as Uniswamp. Unfortunately, approximately six weeks ago the Evil King Gremplin descended upon Uniswamp and dispatched the CrypToadz leader Colonel Floorbin, exiling him to Gooch Island. Following the fall of Colonel Floorbin, the Evil King Gremplin declared his divine right to rule over all CrypToadz in the Metaverse. The Evil King Gremplin's first order was to rename Uniswamp to GREMPLAND. The Evil King Gremplin rules over the CrypToadz along with his army of Fronkz. The Toadz have been known to refer to the Evil King Gremplin's reign as "THE CROAKENING". Help rid the Toadz of the rule of Evil King Gremplin by choosing how many to free during minting.</p>
                 </div>
-                <div className='img-container  col-md-4 gridImage'>
+                <div className='img-container gridImage'>
                     <div className='imgBox'>
                         <img src='/images/401.png' className='about-img' alt='401'/>
                     </div>
@@ -31,12 +31,12 @@ function Home() {
                 </div>
             </div>
             <div className='gremplin d-flex align-items-center justify-content-center'>
-                <div className='img-container col-md-4'>
+                <div className='img-container'>
                     <div className=''>
                         <img src='/images/temp_gremp.png' className='gremplin-img' alt='gremplin'/>
                     </div>
                 </div>
-                <div className='text-container col-md-6'>
+                <div className='text-container'>
                     <h1 className="text-uppercase">Gremplin</h1>
                     <p>On a January evening in early 2021, Gremplin scurried through dark vents and damp pipes after making two wrong turns back home to the depths of his hole. When he finally surfaced, trapped in the chaotic energy of the metaverse, he scrambled to keep pace. He crafted spinning boxes to fit in with the crowd, and drew rotating residents of his homeland to help people understand where he’s from. Eventually, he took to drawing residents of this new land to pass the time. Finally feeling at home, he began to seek out ways to help build in this place. He’s just doing what he’s always done - drawing residents of lands, be they Nouns or Toadz - just trying to find his way back to his hole.</p>
                 </div>
